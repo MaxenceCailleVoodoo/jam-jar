87c1e8ff383a16e414271bdd58c45e9e2ad3d74b
Quentin Laurent <quentin.laurent@voodoo.io>
2026-05-26 18:59:05 +0200

    iteration 6

--- src/view/BootScene.js
+++ src/view/BootScene.js
@@ -6,6 +6,7 @@ export class BootScene extends Phaser.Scene {
   preload() {
     this.load.audio('bgm', 'assets/audio/techno-bgm.mp3');
     this.load.audio('jam-explosion', 'assets/audio/jam-explosion.wav');
+    this.load.image('toast-slice', 'assets/toast-slice.png');
   }
 
   create() {
